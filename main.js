463b98db2397b9701cac9c418f70f901182272b6
hkmusameh01 <abdalhakem1226@gmail.com>
2022-08-03 12:21:52 +0300

    create map and style it Relates #11

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
+let marker;
+// Calling the map on page reloading on lon: 0 and lat: 0;
+createMap();
+
+
 const key = "776f41f2e73c416078fa91fabe5ea7b4";
 searchBtn.addEventListener("click", () => {
   if (searchInput.value) {
     loading.setAttribute("class", "hidden");
   }
   // const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${key}&units=metric`;
-  const url =`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchInput.value}?unitGroup=metric&key=VPA2TH3SE9TJ9UYGQQBL9F33K&contentType=json`
-  fetch(url, buildDom ,week);
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchInput.value}?unitGroup=metric&key=VPA2TH3SE9TJ9UYGQQBL9F33K&contentType=json`;
+  fetch(url, buildDom, week);
+  fetch(url, updateMarker);
 });
 
-function fetch(url, cb ,cb2) {
+function fetch(url, cb, cb2) {
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = () => {
     if (xhr.readyState === 4) {
@@ -19,10 +25,9 @@ function fetch(url, cb ,cb2) {
         cb2(data);
         information.classList.remove("hidden");
       } else {
-        loading.innerHTML =`<br><br>error ${xhr.status}<br><br>  Please enter the correct country or city name` ;
+        loading.innerHTML = `<br><br>error ${xhr.status}<br><br>  Please enter the correct country or city name`;
         loading.classList.remove("hidden");
-        information.classList.add("hidden")
-
+        information.classList.add("hidden");
       }
     }
   };
@@ -30,10 +35,24 @@ function fetch(url, cb ,cb2) {
   xhr.send();
 }
 
-
-function week(data){
-  for(let i=0;i<7;i++){
-    console.log(data.days[i])
-    buildDomWeakSection(data.days[i])
+function week(data) {
+  for (let i = 0; i < 7; i++) {
+    console.log(data.days[i]);
+    buildDomWeakSection(data.days[i]);
   }
+}
+
+function createMap() {
+  // Making a map and tiles
+  const map = L.map("map").setView([0, 0], 6);
+
+  let tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+  let attribution = "© OpenStreetMap";
+  L.tileLayer(tileUrl, attribution).addTo(map);
+
+  marker = L.marker([0,0]).addTo(map);
+}
+
+function updateMarker(data) {
+  marker.setLatLng([data.latitude, data.longitude])
 }
