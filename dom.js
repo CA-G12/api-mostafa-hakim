7b661d0cf655854c5e96153f628a8d2167ba3440
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-08-03 09:46:31 +0300

    create wheather during week section and set data from api and style it relates #9

--- dom.js
+++ dom.js
@@ -2,25 +2,81 @@ const searchInput = document.querySelector(".input-search");
 const searchBtn = document.querySelector(".search-btn");
 const loading = document.querySelector(".loading");
 const information = document.querySelector(".information");
-
-
+const daysContiner = document.querySelector(".days-continer");
 
 function buildDom(data) {
   let cityName = document.querySelector(".city-name");
-  cityName.textContent = `Weather in ${data.name}`;
+  cityName.textContent = `Weather in ${data.address}`;
   let temp = document.querySelector(".temp");
-  temp.textContent = `${data.main.temp}°C`;
+  temp.textContent = `${data.currentConditions.temp}°C`;
   let icon = document.querySelector(".icon");
-  icon.textContent = data.weather.icon;
+  icon.textContent = data.currentConditions.icon;
   let description = document.querySelector(".description");
-  description.textContent = data.weather[0].description;
+  description.textContent = data.description;
   let humidity = document.querySelector(".humidity");
-  humidity.textContent = `Humidity: ${data.main.humidity}%`;
+  humidity.textContent = `Humidity: ${data.currentConditions.humidity}%`;
   let wind = document.querySelector(".wind");
-  wind.textContent = `Wind speed: ${data.wind.speed} km/h`;
+  wind.textContent = `Wind speed: ${data.currentConditions.windspeed} km/h`;
+  let mainContainer = document.querySelector("main");
+  mainContainer.style.backgroundImage = `url("https://source.unsplash.com/1600x900/?${data.name}")`;
+  console.log(data.name);
+}
 
-  
-  let mainContainer = document.querySelector('main');
-  mainContainer.style.backgroundImage = `url("https://source.unsplash.com/1600x900/?${data.name}")`
-  console.log(data.name)
+function buildDomWeakSection(data) {
+  console.log("tesssssssssssst", data);
+  let dayCard = document.createElement("div");
+  dayCard.setAttribute("class", "day");
+  daysContiner.appendChild(dayCard);
+
+  let dateTime = document.createElement("h2");
+  dateTime.setAttribute("class", "date-time");
+  dateTime.textContent = data.datetime;
+  dayCard.appendChild(dateTime);
+
+  let temp = document.createElement("h1");
+  temp.setAttribute("class", "temp");
+  temp.textContent = `${data.temp}°C`;
+  dayCard.appendChild(temp);
+
+  let iconDescriptionContiner = document.createElement("div");
+  iconDescriptionContiner.setAttribute("class", "icon-description-continer");
+  dayCard.appendChild(iconDescriptionContiner);
+
+  let icon = document.createElement("img");
+  icon.setAttribute("class", "icon");
+  icon.textContent = data.icon;
+  iconDescriptionContiner.appendChild(icon);
+
+  let description = document.createElement("p");
+  description.setAttribute("class", "description");
+  description.textContent = data.description;
+  iconDescriptionContiner.appendChild(description);
+
+  let humidity = document.createElement("p");
+  humidity.setAttribute("class", "humidity");
+  humidity.textContent = `Humidity: ${data.humidity}%`;
+  dayCard.appendChild(humidity);
+
+  let wind = document.createElement("p");
+  wind.setAttribute("class", "wind");
+  wind.textContent = `Wind speed: ${data.windspeed} km/h`;
+
+  dayCard.appendChild(wind);
+}
+
+{
+  /* <div class="day">
+<h2 class="date-time">3/8/2022</h2>
+<h1 class="temp">36°C</h1>
+<div class="icon-description-continer">
+  <img
+    class="icon"
+    src="https://openweathermap.org/img/wn/01d.png"
+    alt=""
+  />
+  <p class="description">Clear Sky</p>
+</div>
+<p class="humidity">Humidity: 50%</p>
+<p class="wind">Wind Speed:5.13 km/h</p>
+</div> */
 }
--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-
 const key = "776f41f2e73c416078fa91fabe5ea7b4";
 searchBtn.addEventListener("click", () => {
   if (searchInput.value) {
     loading.setAttribute("class", "hidden");
   }
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${key}&units=metric`;
-  fetch(url, buildDom);
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${key}&units=metric`;
+  const url =`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchInput.value}?unitGroup=metric&key=VPA2TH3SE9TJ9UYGQQBL9F33K&contentType=json`
+  fetch(url, buildDom ,week);
 });
 
-function fetch(url, cb) {
+function fetch(url, cb ,cb2) {
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = () => {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
         let data = JSON.parse(xhr.responseText);
-        console.log(xhr.status);
+        console.log(data);
         cb(data);
+        cb2(data);
         information.classList.remove("hidden");
       } else {
         loading.innerHTML =`<br><br>error ${xhr.status}<br><br>  Please enter the correct country or city name` ;
@@ -27,4 +28,12 @@ function fetch(url, cb) {
   };
   xhr.open("GET", url);
   xhr.send();
+}
+
+
+function week(data){
+  for(let i=0;i<7;i++){
+    console.log(data.days[i])
+    buildDomWeakSection(data.days[i])
+  }
 }
